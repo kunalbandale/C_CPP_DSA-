43645353c7c2221920e381a80a17f441302344a2
Kunal Bandale <bandalekunal@gmail.com>
2023-10-05 18:49:37 +0530

    Add files via upload

---
+++ CPP/BATH.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t , x , y;
+    
+    cin >> t;
+    
+    while(t--) {
+        cin >> x >> y;
+        
+        cout << x/(y*2) << endl;
+    }
+	return 0;
+}
