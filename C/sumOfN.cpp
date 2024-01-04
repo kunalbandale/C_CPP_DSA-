2a1554479ed51b4d6ba32a1c42e304bd4035daa4
Kunal Bandale <bandalekunal@gmail.com>
2024-01-04 23:39:24 +0530

    Add files via upload

---
+++ C/sumOfN.cpp
@@ -0,0 +1,17 @@
+//sum of n natural numbers
+
+#include<iostream>
+ using namespace std;
+ 
+ int main() {
+ 	// n*(n+1)/2
+ 	int n;
+ 	
+ 	cout << "Enter Any Integer" << endl;
+ 	cin >> n;
+ 	
+ 	cout << "Sum of N natural No. is " << n*(n+1)/2;
+ 	
+ 	
+ 	
+ }
