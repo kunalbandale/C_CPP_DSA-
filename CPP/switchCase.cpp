bff187f3f4b772ec066e2a08cf2bf84308b25a7c
Kunal Bandale <bandalekunal@gmail.com>
2024-01-08 00:04:22 +0530

    Add files via upload

---
+++ CPP/switchCase.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+ using namespace std;
+ 
+ int main() {
+ 	int x = 2;
+ 	
+ 	switch(x){
+ 		case 1: cout << "One";
+ 		break;
+ 		
+ 		case 2: cout << "Two" ;
+ 		break;
+ 		
+ 		default: cout << "Invalid Number";
+	 }
+ 	return 0;
+ }
