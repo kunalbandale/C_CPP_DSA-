804385eab3b1332e4fdf8ba609874215ecaed4f5
Kunal Bandale <bandalekunal@gmail.com>
2024-01-14 23:59:14 +0530

    Add files via upload

---
+++ CPP/ArrayDecl.cpp
@@ -0,0 +1,15 @@
+// array declaration and initalization
+#include <iostream>
+ using namespace std;
+ 
+ int main() {
+ 	
+ 	int a[10] = {3,3,4,5,6,56,5,4,1};
+ 	
+ 	cout << a[3];
+	cout << a[8];
+	cout << a[2];
+	cout << a[0];	
+ 	
+ 	return 0;
+ }
