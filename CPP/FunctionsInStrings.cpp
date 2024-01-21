7505b88b3db0851e902dc757279a525e478371a6
Kunal Bandale <bandalekunal@gmail.com>
2024-01-21 23:17:02 +0530

    Add files via upload

---
+++ CPP/FunctionsInStrings.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstring>
+ using namespace std;
+ 
+ int main() {
+ 	
+ 	char s[50] = "SGGS,NANDED";
+ 	cout << strlen(s) << endl;
+ 	
+ 	// ptr
+ 	
+ 	char *str;
+ 	
+ 	cout << "Enter a string" ;
+ 	cin >> str;
+ 	
+ 	cout << "Lenght" << strlen(str) << endl;
+ 	
+ 	
+ 	
+ 	
+ 	return 0;
+ 
+	 
+	  }
