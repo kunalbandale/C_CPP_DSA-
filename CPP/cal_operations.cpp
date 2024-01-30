62db361133c80e4bc9633acd33e98a85a29febb7
Kunal Bandale <bandalekunal@gmail.com>
2024-01-30 20:25:10 +0530

    Add files via upload

---
+++ CPP/cal_operations.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+ using namespace std;
+ 
+ int add(int a , int b) {
+ 	return a + b;
+ }
+ int sub(int a , int b) {
+ 	return a-b;
+ }
+ int mul(int a , int b) {
+ 	return a*b;
+ }
+ 
+ int main() {
+// 	
+	cout << add(4,5) << endl;
+	cout << sub(5,76) << endl;
+	cout << mul(4,5) << endl;
+	
+	return 0;
+	
+ }
