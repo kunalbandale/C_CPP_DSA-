5a9b46e7a9d8cac146ccf91fbb313e81e95068a9
kunalbandale <bandalekunal@gmail.com>
2024-02-15 00:01:02 +0530

    classes and objects in cpp

---
+++ CPP/classes-objects.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+ using namespace std;
+ 
+ class Rectangle {
+ 	public:
+ 		int length;
+ 		int breadth;
+ 		
+ 		int area(){
+ 			return length * breadth;
+		 }
+		 int perimeter() {
+		 	return 2*(length + breadth);
+		 }
+ };
+ 
+ int main() {
+ 	Rectangle r1,r2;
+ 	r1.length = 10;
+ 	r1.breadth = 32;
+ 	
+ 	cout << r1.area() << endl;
+ 	cout << r1.perimeter() << endl;
+ 	
+ }
